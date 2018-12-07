521b7aaecfeeb08c45882b16ae52f5d4483d7c70
Dennis Oba <oba.dennis@gmail.com>
2018-12-06 21:29:12 -0500

    Added filter bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Footer.scss';
 
 function Footer(props) {
 	return (
-		<footer>
+		<footer className="Footer__Component">
 			Data provided by Marvel. © 2014 Marvel
 		</footer>
 	);
